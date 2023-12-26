28648e2f4f77ecbe1ad31b4d66b5ba319f724cd8
Hyhiihyhyhuhu <hychanhtran@gmail.com>
2023-12-27 00:10:38 +0700

    LHP: FINAL PROJECT - MC4CODING _ CoTAI

--- pages/3 - FINDING SOMEONE.py
+++ pages/3 - FINDING SOMEONE.py
@@ -19,7 +19,7 @@ st.markdown(
    unsafe_allow_html=True)
 
 def show(col, i):
-    col.header(f':grey[Matching {i+1}]')
+    col.header(f':grey[Matching {i}]')
     col.image(similar_img[i], width=280)
     col.subheader((round(score[i]*100),2) + '%')
 
