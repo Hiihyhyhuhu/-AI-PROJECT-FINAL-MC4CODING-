cff6ab1a84d00e2f293459816b81f7ad94793b92
Hyhiihyhyhuhu <hychanhtran@gmail.com>
2023-12-26 22:47:48 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -10,10 +10,10 @@ for file in ["style/pic2.JPG", "style/pic3.JPG", "style/pic4.JPG"]:
 st.header(":grey[**FINDING ONE'S LOST - ONCE LOST**]", help='Click on the sections below to get started.')
 col1, col2 = st.columns([0.45,0.55])
 with col1:
-   st.markdown(f"[![Foo]({images[0]})](http://localhost:8501/GET_TO_KNOW_US)")
-   st.markdown(f"[![Foo]({images[1]})](http://localhost:8501/GET_TO_KNOW_YOU)")
+   st.markdown(f"[![Foo]({images[0]})](http://hiihyhyhuhu/-ai-project-final-mc4coding-/GET_TO_KNOW_US)")
+   st.markdown(f"[![Foo]({images[1]})](http://hiihyhyhuhu/-ai-project-final-mc4coding-/GET_TO_KNOW_YOU)")
 with col2:
-   st.markdown(f"[![Foo]({images[2]})](http://localhost:8501/FINDING_SOMEONE)")
+   st.markdown(f"[![Foo]({images[2]})](http://hiihyhyhuhu/-ai-project-final-mc4coding-/FINDING_SOMEONE)")
 with st.sidebar:
    if st.button('Where to go'):
       msg = st.toast("Find us at GET TO KNOW US", icon='🔍')
