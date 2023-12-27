2d04ecd7cfce8d470494466444ce5cd6ea8739be
Hyhiihyhyhuhu <hychanhtran@gmail.com>
2023-12-27 16:09:17 +0700

    LHP: FINAL PROJECT - MC4CODING _ CoTAI

--- pages/3 - FINDING SOMEONE.py
+++ pages/3 - FINDING SOMEONE.py
@@ -88,7 +88,7 @@ with tab1:
                 my_bar.progress(percent_complete + 1, text='Operation in progress. Please wait.')
             time.sleep(1)
         result = DeepFace.find(img_path = "style/sample_img.png", db_path = 'images', enforce_detection = False)
-        st.write(len(result))
+        result = result[0]
         result = result.reshape((-1,10))
         result = np.flip(result)
         similar_img, score = result[:10,0], result[:10,9]
